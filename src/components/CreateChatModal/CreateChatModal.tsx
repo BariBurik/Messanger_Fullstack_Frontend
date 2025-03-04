e28546ba5e5e3f7c83d8267c3dd33181181620e3
Oleg122 <joeburen122@gmail.com>
2025-03-04 12:11:55 +0300

    build12

--- src/components/CreateChatModal/CreateChatModal.tsx
+++ src/components/CreateChatModal/CreateChatModal.tsx
@@ -50,7 +50,7 @@ function CreateChatModal({isOpen, setIsOpen}: CreateChatModalProps) {
 
         if (participants) {
             setIsOpen(false)
-            console.log('dsd' + participants)
+            console.log(participants)
             createChatroom({
                 name: chatName, 
                 users: participants,
